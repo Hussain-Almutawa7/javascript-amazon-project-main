2332a4a08d909a2710f0015b9c3c7bd68f87cc53
= <=>
2025-08-22 18:18:08 +0300

    Make delivery options interactive.

--- data/cart.js
+++ data/cart.js
@@ -4,11 +4,11 @@ if (!cart) {
   cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
-    deliveryOptionId: 1
+    deliveryOptionId: "1"
   }, {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1,
-    deliveryOptionId: 2
+    deliveryOptionId: "2"
   }];
 }
 
@@ -31,7 +31,7 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: amountAdded,
-      deliveryOptionId: 1
+      deliveryOptionId: "1"
     });
   }
 
@@ -67,5 +67,11 @@ export function updateQuantity(productId, newQuantity) {
   const matchingItem = cart.find(item =>  item.productId === productId);
   matchingItem.quantity = newQuantity;
 
+  saveToStorage();
+}
+
+export function updateDeliverOption(productId, deliveryOptionId) {
+  const matchingItem = cart.find(cartItem => productId === cartItem.productId);
+  matchingItem.deliveryOptionId = deliveryOptionId;
   saveToStorage();
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart, updateCartQuantityNumber, updateQuantity} from '../data/cart.js';
+import {cart, removeFromCart, updateCartQuantityNumber, updateQuantity, updateDeliverOption} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
@@ -83,7 +83,9 @@ document.addEventListener("DOMContentLoaded", () => {
       const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
       
       html += `
-        <div class="delivery-option">
+        <div class="delivery-option js-delivery-option"
+        data-product-id = "${matchingProduct.id}"
+        data-delivery-option-id = "${deliveryOption.id}">
           <input type="radio" ${isChecked ? "checked" : ""}
             class="delivery-option-input"
             name="delivery-option-${matchingProduct.id}">
@@ -177,9 +179,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
   document.querySelector(".js-checkout-link").innerHTML = `${updateCartQuantityNumber()} items`;
 
-   /*
-    const today = dayjs();
-    const firstDileveryDate = today.add(7, 'days');
-    console.log(firstDileveryDate.format("dddd, MMMM D"));
-    */
+  document.querySelectorAll(".js-delivery-option").forEach(option => {
+    option.addEventListener("click", () => {
+      const {productId, deliveryOptionId} = option.dataset;
+      updateDeliverOption(productId, deliveryOptionId);
+    })
+  });
+
 })
