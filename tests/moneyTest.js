46747a99a54c3229d616a1527273a524aefc15a1
= <=>
2025-09-02 00:29:24 +0300

    Create automated tests for formatCurrency

---
+++ tests/moneyTest.js
@@ -0,0 +1,36 @@
+import formatCurrency from "../scripts/utils/money.js";
+
+console.log("Test suit: formatCurrencey");
+
+//Basic Test Case
+console.log("Converts cents into dollar");
+
+if (formatCurrency(2095) === "20.95") {
+    console.log("passed");
+} else {
+    console.log("failed");
+}
+
+//Edge Cases
+console.log("works with 0")
+
+if (formatCurrency(0) === "0.00") {
+    console.log("passed");
+} else {
+    console.log("failed");
+}
+
+console.log("round up to the nearest cents");
+
+if (formatCurrency(2000.5) === "20.01") {
+    console.log("passed");
+} else {
+    console.log("failed");
+}
+
+if (formatCurrency(2000.4) === "20.00") {
+    console.log("passed");
+} else {
+    console.log("failed");
+}
+
