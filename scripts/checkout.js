4df286c7a12cbbed526aec1f7f624f967b7d1b3c
= <=>
2025-09-22 22:19:20 +0300

    Convert loadProducts from XHR to fetch

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,21 +1,27 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/CheckoutHeader.js";
-import { loadProducts } from "../data/products.js";
+import { loadProducts, loadProductsFetch } from "../data/products.js";
 //import ".././data/cart-oop.js"
 //import ".././data/cart-class.js";
 //import "../data/backend-practice.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  new Promise((resolve) => {
-    loadProducts(() => {
-      resolve();
-    });
-  }).then(() => {
+  /*
+  loadProductsFetch().then(() => {
     renderCheckoutHeader();
     renderOrderSummary();
     renderPaymentSummary();
   });
+  */
+
+  Promise.all([
+    loadProductsFetch().then(() => {
+      renderCheckoutHeader();
+      renderOrderSummary();
+      renderPaymentSummary();
+    }),
+  ]);
 
   /*
   loadProducts(() => {
